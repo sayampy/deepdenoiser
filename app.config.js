e433a058b25661aca630dfba2e560c7234de829f
sayampy <sayampy.code@gmail.com>
2026-03-20 20:25:49 +0530

    preview release 1

--- app.config.js
+++ app.config.js
@@ -1,10 +1,14 @@
 const IS_DEV = process.env.APP_VARIANT;
-export default {
-  name: IS_DEV ? "DeepDenoiser (Dev)" : "DeepDenoiser",
-  slug: "deepdenoiser",
-  android: {
-    package: IS_DEV
-      ? "com.sayampy.deepdenoiser.dev"
-      : "com.sayampy.deepdenoiser",
-  },
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    name: IS_DEV ? "DeepDenoiser (Dev)" : "DeepDenoiser",
+    slug: "deepdenoiser",
+    android: {
+      ...config.android,
+      package: IS_DEV
+        ? "com.sayampy.deepdenoiser.dev"
+        : "com.sayampy.deepdenoiser",
+    },
+  };
 };
